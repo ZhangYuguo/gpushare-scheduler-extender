174b20bce0a3f48ba321fdbdbabb787e302af186
zhangyuguo <zhangyuguo@chinamobile.com>
2020-04-19 18:59:57 +0800

    自包含

--- cmd/main.go
+++ cmd/main.go
@@ -9,10 +9,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/gpushare"
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/routes"
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/scheduler"
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/utils/signals"
+	"github.com/zhangyuguo/gpushare-scheduler-extender/pkg/gpushare"
+	"github.com/zhangyuguo/gpushare-scheduler-extender/pkg/routes"
+	"github.com/zhangyuguo/gpushare-scheduler-extender/pkg/scheduler"
+	"github.com/zhangyuguo/gpushare-scheduler-extender/pkg/utils/signals"
 	"github.com/comail/colog"
 	"github.com/julienschmidt/httprouter"
 
